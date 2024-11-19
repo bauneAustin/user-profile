de7a2892138d84664ea385d01ca6c6381cb45769
bauneAustin <abaune.dev@gmail.com>
2024-11-19 09:11:57 -0700

    fix linting error

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
     return (
         <main className="text-main-950">
             <h1 className={`mb-4 text-xl md:text-2xl`}>
-                Austin's Posts
+                All Posts
             </h1>
             <div className="mt-6 h-full w-full flex flex-col">
                 <BlogTable />
