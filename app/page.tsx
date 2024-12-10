a7b44dbeb08bd7b3ec469fb8dfc22990f86df199
bauneAustin <abaune.dev@gmail.com>
2024-12-09 22:16:07 -0700

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
-// import Image from "next/image";
+'use client';
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const onClick = () => {
+    redirect('/dashboard');
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          Austin Baune
+    <div className="flex flex-col h-full w-full">
+      <div>
+        <h1 className="text-6xl mb-24 text-main-950 text-center">
+          Welcome to the Test Dashboard
+        </h1>
+      </div>
+      <main className="flex flex-col w-full">
+        <div className="flex justify-center self-center w-full sm:self-center">
+          <Image className="rounded-full" src={'/profile.png'} alt='profile-pic' width={450} height={450}/>
+        </div>
+        <div className="flex w-full justify-center self-center">
+          <button onClick={onClick} className="p-4 mt-[80px] bg-main-700 rounded w-[200px] align-middle text-slate-100 font-semibold text-xl cursor-pointer hover:bg-main-300 hover:text-blue-950">
+            Enter
+          </button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        Footer
+      <footer className="flex h-full w-full self-center justify-center">
+        <h2 className="self-end text-sm">
+          Austin Baune
+        </h2>
       </footer>
     </div>
   );
--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -1,8 +1,13 @@
+'use client';
+import { redirect } from "next/navigation";
 import Pill from "./pill/pill";
 export default function Sidebar() {
+    const onClick = () => {
+        redirect('/');
+    }
     return (
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
-            <div className="bg-main-950 flex h-48 flex-col rounded w-full text-white text-9xl justify-end text-center">
+            <div onClick={onClick} className="bg-main-950 flex h-48 flex-col rounded w-full text-white text-9xl justify-end text-center cursor-pointer">
                 AB
             </div>
             <div className="flex grow flex-col h-full bg-main-50 py-2 mt-3 md:flex-col md:space-x-0 md:space-y-2">
